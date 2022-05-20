13517b58b117ef37101f58df1ec3bfe65922dc92
nzeremc <52140404+nzeremc@users.noreply.github.com>
2022-05-19 19:13:38 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,11 @@
+###################### state File ################
 
+terraform {
+  backend "s3" {
+    bucket               = "myrdsbucket"
+    region               = "us-east-1"
+    key                  = "Rds"
+    #dynamodb_table       = ""
+    encrypt              = true
+  }
+}
